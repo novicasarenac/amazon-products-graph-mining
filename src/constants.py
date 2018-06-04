3009d5b446711698082d0fb1f794c4f8f453a590
novicasarenac <nolesarenac@yahoo.com>
2018-06-05 00:40:48 +0200

    Path changes

--- src/constants.py
+++ src/constants.py
@@ -5,5 +5,5 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATASET_PATH = BASE_DIR + '/data/meta_Apps_for_Android.json'
 IMPORTABLE_NODES = BASE_DIR + '/data/nodes.csv'
 IMPORTABLE_EDGES = BASE_DIR + '/data/edges.csv'
-CLUSTERS_DATA = BASE_DIR + '/data/clusters.csv'
-CLIQUE_DATA = BASE_DIR + '/data/clique.csv'
+CLUSTERS_DATA = BASE_DIR + '/clusters_visualization/template/clusters.json'
+CLIQUE_DATA = BASE_DIR + '/largest_clique_visualization/template/clique.json'
